eb65702d86b6e7e1a7262333441d5a6e631deda7
GoshkaKap <141616168+GoshkaKap@users.noreply.github.com>
2023-12-17 01:24:29 +0500

    бот_вк.py

--- бот_вк.py
+++ бот_вк.py
@@ -2,36 +2,41 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import requests
 import nlpcloud
+import time
 
-bot = vk_api.VkApi(token='blablabla')
+bot = vk_api.VkApi(token='blabla')
 session_api = bot.get_api()
 longpool = VkLongPoll(bot)
 
 def send_some_msg(id, some_text):
-    bot.method("messages.send", {"user_id":id, "message":some_text,"random_id":0})
+    text = ''
+    for i in some_text :
+        text += i
+
+    bot.method("messages.send", {"user_id":id, "message":text,"random_id":0})
 
 for event in longpool.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
             msg = event.text.lower()
-            id = event.user_id
+            id  = event.user_id
 
-            first_check = {
-                            'api_version' : 'v1',
-                            'main_token' : 'bla',
+            first_check =  {
+                            'api_version'   : 'v1',
+                            'main_token'    : 'blabla',
                             'query_details' : 
                                 {
-                                'source_token' : 'bla', 
-                                'text' : msg,
+                                'source_token' : 'blabla', 
+                                'text'         : msg,
                                 'dictionaries' :
                                     {
-                                    'rus' : 
-                                        [
-                                         'heavy',
-                                         'expletive'
-                                        ]
+                                     'rus' : 
+                                            [
+                                             'heavy',
+                                             'expletive'
+                                            ]
                                     },
-                                'deep_check' : 'no',
+                                'deep_check'     : 'no',
                                 'translit_check' : 'yes'
                                 }
                           }
@@ -39,61 +44,66 @@ for event in longpool.listen():
             res = requests.post('https://lf.statusnick.com/api/text/check', json = first_check).json()
             try: 
                 if res['result']['final']['check']['check_result'] != [] :
-                    send_some_msg(id, 'Ваше обращение не принято к рассмотрению, так как оно содержит ненормативную лексику.')
+                    send_some_msg(id, ['Ваше обращение не принято к рассмотрению, так как оно содержит ненормативную лексику.'])
                 else :
                     client = nlpcloud.Client("bart-large-mnli-yahoo-answers", "blabla")
                     res = client.classification(msg, labels=['собака', 'бродячие', 
                                                              'газ', 'вода', 'труба', 'пар', 'лифт', 'мусор',
-                                                             'расселение'], multi_class=True)
-                    res2 = client.classification(msg, labels=['пёс', 'бешенные', 'животные',
+                                                             'расселение', 'расселят'], multi_class=True)
+                    res2 = client.classification(msg, labels=['кошка', 'крыса', 'животные',
                                                               'дороги', 'ямы', 
                                                               'прорвало', 'электричество', 'крыша'
                                                               'ветхое', 'фонд'], multi_class=True)
-                    #res['scores'] += res2['scores']
-                    #res['labels'] += res['labels']
+
+                    animals = sum(res['scores'][0:2] + res2['scores'][0:3])   / len(res['scores'][0:2] + res2['scores'][0:3])                            
+                    ways    = sum(res2['scores'][3:5])                        / len(res2['scores'][3:5])                                                   
+                    streets = sum(res['scores'][2:8] + res2['scores'][5:8])   / len(res['scores'][2:8] + res2['scores'][5:8])         
+                    home    = sum(res['scores'][8:10] + res2['scores'][8:10]) / len([res['scores'][8:10]] + [res2['scores'][8:10]])\
                     
-                    if max(res['scores']) - min(res['scores']) <= 0.22 :
-                        problem = True
+                    scores = [animals, ways, streets, home]
+                    print(msg, max(scores) - min(scores))
+                    if max(scores) - min(scores) <= 0.0 :
                         hello = ['здравствуйте', 'привет', 'добр' ]
-                        bye = ['до свид', 'пока']
+                        bye   = ['до свид', 'пока']
+
+                        problem = True
                         for i in range(0, len(hello)-1) :
                             if -1 < msg.find(hello[i]) < len(hello) :
                                     problem = False
-                                    send_some_msg(id, 'Здравствуйте! \nЯ - бот-помощник главы региона и могу ответить на интересующие Вас вопросы')
+                                    send_some_msg(id, ['Здравствуйте! \n',
+                                                       'Я - бот-помощник главы региона и могу ответить на интересующие Вас вопросы'])
                                     break
                             elif i < len(bye):
                                 if -1 < msg.find(bye[i]) < len(bye) :
                                     problem = False
-                                    send_some_msg(id, 'До свидания! \nНадеюсь, мы смогли решить Ваш вопрос. Буду рад помочь Вам снова')
+                                    send_some_msg(id, ['До свидания! \n',
+                                                       'Надеюсь, мы смогли решить Ваш вопрос. Буду рад помочь Вам снова'])
                                     break
                         if problem :
-                            send_some_msg(id, 'Мы Вас не поняли \nПопробуйте сформулировать Вашу проблему иначе')
-                    else :
-                        index_max = res['scores'].index(max(res['scores']))
-                        answer = res['labels'][index_max]
-                        score = res['scores'][index_max]
+                            send_some_msg(id, ['Мы Вас не поняли \n',
+                                               'Попробуйте сформулировать Вашу проблему иначе'])
+                    else :                       
+                        index_max = scores.index(max(scores))
 
-                        animals = ['собака', 'бродячие', 'пёс', 'бешенные', 'животные']
-                        ways =    ['дороги и ямы', 'дороги', 'ямы']
-                        streets = ['газ', 'вода', 'труба', 'пар', 'лифт', 'мусор', 'прорвало', 'электричество', 'крыша']
-                        home =    ['расселение', 'ветхое', 'фонд']
-                        
-                        for i in range(0, len(streets)-1) :
-                            if i < len(animals) :
-                                if answer == animals[i] :
-                                    send_some_msg(id, 'Благодарим за обращение! \nВ Вашем городе уже работают службы по отлову бродячих животных\npublication.pravo.gov.ru/document/1401202308240001?index=1')
-                                    break
-                            elif i < len(ways) :
-                                if answer == ways[i] :
-                                    send_some_msg(id, 'Благодарим за обращение! \nВ регионе уже активно ведутся работы по улучшению дорог \npublication.pravo.gov.ru/document/1400202308250001')
-                                    break
-                            if answer == streets[i]:
-                                    send_some_msg(id, 'Большое спасибо за Ваше сообщение! \nМы передадим его оператору, но для ускорения решения вопроса рекомендуем Вам позвонить в единую диспетчерскую службу по телефону +7(41136)4-41-12')
-                                    break
-                            elif i < len(home) :
-                                if answer == home[i] :
-                                    send_some_msg(id, 'Благодарим за обращение! \nВ Вашем районе уже действует программа по расселению ветхого жилья \nwww.алмазный-край.рф/for%20export/1673.pdf')
-                                    break
-            except: 
-                send_some_msg(id, 'Вы вызвали сбой в системе! \nВероятно, Вы использовали стикеры или слишком часто отправляли сообщения')
+                        if   index_max == 0 :
+                            send_some_msg(id, ['Благодарим за обращение! \n',
+                                               'В Вашем городе уже работают службы по отлову бродячих животных \n',
+                                               'publication.pravo.gov.ru/document/1401202308240001?index=1'])
+                        elif index_max == 1 :
+                            send_some_msg(id, ['Благодарим за обращение! \n',
+                                               'В регионе уже активно ведутся работы по улучшению дорог \n',
+                                               'publication.pravo.gov.ru/document/1400202308250001'])
+                        elif index_max == 2:
+                            send_some_msg(id, ['Большое спасибо за Ваше сообщение! \n',
+                                               'Мы передадим его оператору, но для ускорения решения вопроса рекомендуем Вам позвонить в единую диспетчерскую службу по телефону +7(41136)4-41-12'])
+                        elif index_max == 3 :
+                            send_some_msg(id, ['Благодарим за обращение! \n',
+                                               'В Вашем районе уже действует программа по расселению ветхого жилья \n',
+                                               'www.алмазный-край.рф/for%20export/1673.pdf'])
+            except : 
+                send_some_msg(id, ['Вы вызвали сбой в системе! \n',
+                                   'Бот принимает только текст'])
             
+            send_some_msg(id, ['Мы уведомим Вас, когда Вы снова сможете писать сообщения'])
+            time.sleep(20)
+            send_some_msg(id, ['Теперь мы готовы ответить на Ваш вопрос'])
