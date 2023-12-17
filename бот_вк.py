6402ea85fb2d6712647eba10450474d3fc286e87
GoshkaKap <141616168+GoshkaKap@users.noreply.github.com>
2023-12-17 21:01:25 +0500

    бот_вк.py

--- бот_вк.py
+++ бот_вк.py
@@ -4,9 +4,20 @@ import requests
 import nlpcloud
 import time
 
-bot = vk_api.VkApi(token='blabla')
+bot = vk_api.VkApi(token='vk1.a.I_CYefA8K0q6dx4Lpp0fgdMJuw3mzjS3w06M-JNZG3drTbzoQbENJdrVeCjMKIEqnBJIJHuI8cqAFO6-UpdsYdrzN-p11dF74AU52ZxgwlKuyS-Le-rma1a-JTOyBxbUbZftunVmx5dYS9I67bR23pr06iSuqOGJKdiSqYPu_jvCvorQvI5o9Xd0vzZd8QlGs1r_yllMVY0tS6JhQ3Rkjg')
 session_api = bot.get_api()
 longpool = VkLongPoll(bot)
+
+def what(msg) :
+    client = nlpcloud.Client("bart-large-mnli-yahoo-answers", "2919728073d3aa51ed3e04d4e7eb5153eab05b6d")
+    res = client.classification(msg, labels=['собака', 'бродячие', 
+                                             'газ', 'вода', 'труба', 'пар', 'лифт работает', 'мусор',
+                                             'расселение', 'расселят'], multi_class=True)
+    res2 = client.classification(msg, labels=['кошка', 'крыса', 'животные',
+                                              'плохие дороги', 'ямы', 
+                                              'прорвало', 'электричество', 'крыша'
+                                              'ветхое', 'жильё'], multi_class=True)
+    return([res,res2])
 
 def send_some_msg(id, some_text):
     text = ''
@@ -23,10 +34,10 @@ for event in longpool.listen():
 
             first_check =  {
                             'api_version'   : 'v1',
-                            'main_token'    : 'blabla',
+                            'main_token'    : '2Ws9hR9R1kaZ',
                             'query_details' : 
                                 {
-                                'source_token' : 'blabla', 
+                                'source_token' : '687_66edc5e066eef9b1df044d07a635b469', 
                                 'text'         : msg,
                                 'dictionaries' :
                                     {
@@ -42,27 +53,39 @@ for event in longpool.listen():
                           }
             
             res = requests.post('https://lf.statusnick.com/api/text/check', json = first_check).json()
-            try: 
+            lose = False
+            try :
                 if res['result']['final']['check']['check_result'] != [] :
+                    lose = True
                     send_some_msg(id, ['Ваше обращение не принято к рассмотрению, так как оно содержит ненормативную лексику.'])
-                else :
-                    client = nlpcloud.Client("bart-large-mnli-yahoo-answers", "blabla")
-                    res = client.classification(msg, labels=['собака', 'бродячие', 
-                                                             'газ', 'вода', 'труба', 'пар', 'лифт', 'мусор',
-                                                             'расселение', 'расселят'], multi_class=True)
-                    res2 = client.classification(msg, labels=['кошка', 'крыса', 'животные',
-                                                              'дороги', 'ямы', 
-                                                              'прорвало', 'электричество', 'крыша'
-                                                              'ветхое', 'фонд'], multi_class=True)
+            except KeyError: 
+                lose = True
+                send_some_msg(id, ['Вы вызвали сбой в системе! \n',
+                                   'Бот принимает только текст'])
+            if lose == False :
+                often = False
+                try:
+                    res = what(msg)
+                    res2 = res[1]
+                    res = res[0]
+                except requests.exceptions.HTTPError :
+                    try:
+                        time.sleep(35)
+                        res = what(msg)
+                        res2 = res[1]
+                        res = res[0]
+                    except requests.exceptions.HTTPError :
+                        send_some_msg(id, ['Вы слишком часто отправляете сообщения!'])
+                        often = True
 
+                if often == False :
                     animals = sum(res['scores'][0:2] + res2['scores'][0:3])   / len(res['scores'][0:2] + res2['scores'][0:3])                            
                     ways    = sum(res2['scores'][3:5])                        / len(res2['scores'][3:5])                                                   
-                    streets = sum(res['scores'][2:8] + res2['scores'][5:8])   / len(res['scores'][2:8] + res2['scores'][5:8])         
-                    home    = sum(res['scores'][8:10] + res2['scores'][8:10]) / len([res['scores'][8:10]] + [res2['scores'][8:10]])\
+                    streets = sum(res['scores'][2:8] + res2['scores'][5:8])   / (len(res['scores'][2:8] + res2['scores'][5:8]) - 1)         
+                    home    = sum(res['scores'][8:10] + res2['scores'][8:10]) / len(res['scores'][8:10] + res2['scores'][8:10])
                     
                     scores = [animals, ways, streets, home]
-                    print(msg, max(scores) - min(scores))
-                    if max(scores) - min(scores) <= 0.0 :
+                    if  max(scores) - min(scores) < 0.065:
                         hello = ['здравствуйте', 'привет', 'добр' ]
                         bye   = ['до свид', 'пока']
 
@@ -72,7 +95,7 @@ for event in longpool.listen():
                                     problem = False
                                     send_some_msg(id, ['Здравствуйте! \n',
                                                        'Я - бот-помощник главы региона и могу ответить на интересующие Вас вопросы'])
-                                    break
+                                    break   
                             elif i < len(bye):
                                 if -1 < msg.find(bye[i]) < len(bye) :
                                     problem = False
@@ -100,10 +123,3 @@ for event in longpool.listen():
                             send_some_msg(id, ['Благодарим за обращение! \n',
                                                'В Вашем районе уже действует программа по расселению ветхого жилья \n',
                                                'www.алмазный-край.рф/for%20export/1673.pdf'])
-            except : 
-                send_some_msg(id, ['Вы вызвали сбой в системе! \n',
-                                   'Бот принимает только текст'])
-            
-            send_some_msg(id, ['Мы уведомим Вас, когда Вы снова сможете писать сообщения'])
-            time.sleep(20)
-            send_some_msg(id, ['Теперь мы готовы ответить на Ваш вопрос'])
